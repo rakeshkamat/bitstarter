976b088ab1c8a4dea236c9b4b9e990304ced3e15
Rakesh Kamat <rakeshkamat@gmail.com>
2013-07-16 03:39:55 +0000

    web.js reading from index.html

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buff = fs.readFileSync(index.html);
+  var buff = fs.readFileSync("index.html");
     response.send(buff.toString());
   //response.send('Hello World 2!');
 });
